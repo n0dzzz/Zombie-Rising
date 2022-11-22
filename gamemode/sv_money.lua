56f3c6d3dfcd672e02e7fae8c2d2e9399f782f97
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-11-22 03:34:17 -0500

    Update sv_money.lua

--- gamemode/sv_money.lua
+++ gamemode/sv_money.lua
@@ -1,17 +1,3 @@
-local Meta = FindMetaTable("Player")
-
-function Meta:AddMoney(amount)
-    if self:GetNWInt("MoneyAmount") == 0 then
-        self:SetNWInt("MoneyAmount", amount)
-    end
-
-    self:SetNWInt("MoneyAmount", self:GetMoney() + amount)
-end
-
-function Meta:GetMoney()
-    return self:GetNWInt("MoneyAmount")
-end
-
 hook.Add("OnNPCKilled", "OnKill", function(npc, attacker, inflictor)
-    attacker:AddMoney(100)
+    attacker:SetNWInt("MoneyAmount",attacker:GetNWInt("MoneyAmount") + 100)
 end)
