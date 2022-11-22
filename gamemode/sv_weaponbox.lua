a31a23caa987ee0a9c4c2a73976e43469d7deb01
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-11-22 02:55:56 -0500

    Create sv_weaponbox.lua

---
+++ gamemode/sv_weaponbox.lua
@@ -0,0 +1,29 @@
+util.AddNetworkString("OpenInteraction")
+util.AddNetworkString("RecieveWeapon")
+
+AddCSLuaFile("autorun/server/sv_money.lua")
+include("autorun/shared.lua")
+include("autorun/server/sv_money.lua")
+
+local CanUse = true
+
+hook.Add("PlayerUse", "BoxInteraction", function(ply, ent)
+    if CanUse && ply:GetEyeTrace().Entity:GetModel() == "models/items/ammocrate_ar2.mdl" then
+        net.Start("OpenInteraction")
+        net.Send(ply)
+
+        CanUse = false
+
+        timer.Create("Debounce", 1, 1, function()
+            CanUse = true
+        end)
+    end
+
+end)
+
+net.Receive("RecieveWeapon", function(len, ply)
+    local NetTable = net.ReadTable()
+    
+    ply:Give(tostring(NetTable[1]))
+    ply:RemoveMoney(NetTable[2])
+end)
