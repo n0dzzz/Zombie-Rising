ba4fed58627d10ff3aaf8429a6b6cb35c38f350f
d1stin <62735552+d1stin@users.noreply.github.com>
2022-11-22 02:32:17 -0800

    Add files via upload

---
+++ sv_zombiespawn.lua
@@ -0,0 +1,71 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_hud.lua" )
+
+include( "shared.lua" )
+include( "cl_hud.lua" )
+
+local RoundNumber = 1
+local ZombieDamage = 5
+local ZombieHealth = 50
+local MaxZombies = 10 
+local Zombies = 0
+local SpawnDelay = 5
+
+timer.Create("ZombieSpawn", SpawnDelay, 0, function()
+    local Spawns = ents.FindByClass("info_player_start")
+    local Ran = math.random(#Spawns)
+    local Zombie = ents.Create("npc_fastzombie")
+    local Poison = ents.Create("npc_poisonzombie")
+    local Zombine = ents.Create("npc_zombine")
+
+    if (!IsValid(Zombie))  then return end
+    if (!IsValid(Poison))  then return end
+    if (!IsValid(Zombine))  then return end
+
+    Zombie:SetPos(Spawns[Ran]:GetPos() + Vector(0, 0, math.random( 0, 5)))
+    Zombie:Spawn()
+    Zombie:SetHealth(ZombieHealth * RoundNumber / 2)
+    Zombie:PointAtEntity(Entity(1))
+    Zombie:PointAtEntity(Entity(2))
+
+    if(RoundNumber == 5) then
+        SpawnDelay = 3
+        print(SpawnDelay)
+    end
+    if(RoundNumber == 15) then
+        SpawnDelay = 1.5
+        Zombine:SetPos(Spawns[Ran]:GetPos() + Vector(0, 0, math.random( 0, 25)))
+        Zombine:Spawn()
+        Zombine:PointAtEntity(Entity(1))
+        Zombine:PointAtEntity(Entity(2))
+    end
+    if(RoundNumber == 10) then 
+        Poison:SetPos(Spawns[Ran]:GetPos() + Vector(0, 0, math.random( 0, 25)))
+        Poison:Spawn()
+        Poison:PointAtEntity(Entity(1))
+        Poison:PointAtEntity(Entity(2))
+    end
+    
+    Zombies = Zombies + 1
+end)
+
+hook.Add("Think", "CheckZombies", function()
+    if (Zombies == MaxZombies) then  
+        timer.Pause("ZombieSpawn")        
+        local ZombieLeft = 0
+        for k,v in pairs(ents.GetAll()) do
+            if v:GetClass() == "npc_zombie" then 
+                ZombieLeft = ZombieLeft + 1
+            end
+        end
+        if ZombieLeft == 0 then
+            Zombies = 0
+            RoundNumber = RoundNumber + 1
+            GetConVar("RoundNumberVar"):SetInt(RoundNumber)
+            RunConsoleCommand("say", "Round " .. tostring(RoundNumber))
+            timer.UnPause("ZombieSpawn")
+        end
+            
+    end
+end)
