338d47fe1ed129b2fb5943c334de9a28dedba2de
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-11-22 06:44:22 -0500

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,5 +1,10 @@
 DeriveGamemode( "sandbox" )
 
+AddCSLuaFile( "cl_weaponbox.lua" )
+AddCSLuaFile( "cl_hud.lua" )
+AddCSLuaFile( "cl_init.lua" )
+
+
 GM.Name = "Zombie Surival"
 GM.Author = "GrandmaSocks"
 GM.Email = "grandmasocksbuisness@gmail.com"
