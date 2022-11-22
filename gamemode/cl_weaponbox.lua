5000ea7cd0c59b77a3c2522a2d1155ccd6bb9b0d
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-11-22 05:41:38 -0500

    Update cl_weaponbox.lua

--- gamemode/cl_weaponbox.lua
+++ gamemode/cl_weaponbox.lua
@@ -132,7 +132,7 @@ net.Receive("OpenInteraction", function()
 end)
 
 hook.Add("HUDPaint", "Interaction", function()
-    if tostring(LocalPlayer():GetEyeTrace().Entity) == "[NULL Entity]" then return end
+    if !IsValid(LocalPlayer():GetEyeTrace().Entity) then return end
     
     if LocalPlayer():GetEyeTrace().Entity:GetModel() == "models/items/ammocrate_ar2.mdl" && LocalPlayer():GetPos():Distance(LocalPlayer():GetEyeTrace().Entity:GetPos()) <= 80 then
         draw.DrawText( "Use your interaction key to open the menu! ", "TargetID", ScrW() * 0.5, ScrH() * 0.5, color_white, TEXT_ALIGN_CENTER )
