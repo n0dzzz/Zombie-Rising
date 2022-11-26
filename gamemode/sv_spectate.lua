837b301a2c4aa4738aab6983c1d070de83e64427
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-11-26 02:53:35 -0500

    Create sv_spectate.lua

---
+++ gamemode/sv_spectate.lua
@@ -0,0 +1,26 @@
+util.AddNetworkString("BeginSpectate")
+util.AddNetworkString("EndGame")
+
+include("shared.lua")
+
+local EndGameVar = true
+
+net.Receive("BeginSpectate", function(len,ply)
+    local SpecEnt = net.ReadEntity()
+    if IsValid(SpecEnt) then
+        ply:Spectate(OBS_MODE_FIXED)
+        ply:SpectateEntity(SpecEnt)
+    end
+end)
+
+net.Receive("EndGame",function(len,ply)
+    if EndGameVar then
+        EndGameVar = false
+        for k,v in pairs(player.GetAll()) do
+            v:PrintMessage( HUD_PRINTCENTER, "Game over. You made it to round ".. tostring(GetConVar("RoundNumberVar"):GetInt()))
+            timer.Simple(3, function()
+                RunConsoleCommand("changelevel", game.GetMap())
+            end)
+        end
+    end
+end)
