915a0a5e023d34a1e131e61e593cccfb65809385
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-12-23 18:36:44 -0500

    Create sv_hit.lua

---
+++ gamemode/sv_hit.lua
@@ -0,0 +1,11 @@
+util.AddNetworkString("SendAttacker")
+
+include( "shared.lua" )
+
+hook.Add("EntityTakeDamage", "SendHit", function(target,dmginfo)
+    if !target:IsNPC() then return end
+    if dmginfo:GetAttacker():IsPlayer() then
+        net.Start("SendAttacker")
+        net.Broadcast()
+    end
+end)
