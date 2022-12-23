a76b366f91a95d79046a6aab7e0e85bb8dc9391a
d1stin <62735552+d1stin@users.noreply.github.com>
2022-12-23 00:07:59 -0800

    Add files via upload

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -31,21 +31,21 @@ end )
 hook.Add("HUDPaint", "HUDHOOK", function()
 
 	if (LocalPlayer():Alive()) then
-		draw.RoundedBox(4, 10, surface.ScreenHeight() * 0.75, surface.ScreenWidth() * 0.2, surface.ScreenHeight() * 0.19, Color(222,16,50))
-		draw.RoundedBox(4 + 4, 10 + 2.5, surface.ScreenHeight() * 0.746 + 6, surface.ScreenWidth() * 0.2 - 5, surface.ScreenHeight() * 0.19 - 5, Color(45,45,45))
+		draw.RoundedBox(4, 10, surface.ScreenHeight() * 0.77, surface.ScreenWidth() * 0.2, surface.ScreenHeight() * 0.19, Color(222,16,50))
+		draw.RoundedBox(4 + 4, 10 + 2.5, surface.ScreenHeight() * 0.767 + 6, surface.ScreenWidth() * 0.2 - 5, surface.ScreenHeight() * 0.19 - 5, Color(45,45,45))
 
-		draw.SimpleText(LocalPlayer():GetName(), "HUDFont", 15, surface.ScreenHeight() * 0.76, color_white )
-		draw.SimpleText("Rank: " .. LocalPlayer():GetNWInt("PlayerRank"), "HUDFont", 15, surface.ScreenHeight() * 0.78, color_white )
-		draw.SimpleText("Prestige: " .. LocalPlayer():GetNWInt("PlayerPrestige"), "HUDFont", 15, surface.ScreenHeight() * 0.8, color_white )
-		draw.SimpleText("Money: $" .. LocalPlayer():GetNWInt("MoneyAmount"), "HUDFont", 15, surface.ScreenHeight() * 0.82, color_white )
+		draw.SimpleText(LocalPlayer():GetName(), "HUDFont", 15, surface.ScreenHeight() * 0.775, color_white )
+		draw.SimpleText("Rank: " .. LocalPlayer():GetNWInt("PlayerRank"), "HUDFont", 15, surface.ScreenHeight() * 0.795, color_white )
+		draw.SimpleText("Prestige: " .. LocalPlayer():GetNWInt("PlayerPrestige"), "HUDFont", 15, surface.ScreenHeight() * 0.815, color_white )
+		draw.SimpleText("Money: $" .. LocalPlayer():GetNWInt("MoneyAmount"), "HUDFont", 15, surface.ScreenHeight() * 0.835, color_white )
 
-		draw.RoundedBox(8, 15, surface.ScreenHeight() * 0.85, surface.ScreenWidth() * 0.15, surface.ScreenHeight() * 0.03, Color(222,16,50))
-		draw.RoundedBox(8, 15 + 2.5, surface.ScreenHeight() * 0.846 + 6, surface.ScreenWidth() * 0.15 * (LocalPlayer():Health() / LocalPlayer():GetMaxHealth()) - 5, surface.ScreenHeight() * 0.03 - 5, Color(167, 15,75))
-		draw.SimpleText(LocalPlayer():Health(), "HUDFont", surface.ScreenWidth() * 0.085, surface.ScreenHeight() * 0.853, color_white, TEXT_ALIGN_CENTER )
+		draw.RoundedBox(8, 15, surface.ScreenHeight() * 0.86, surface.ScreenWidth() * 0.15, surface.ScreenHeight() * 0.03, Color(222,16,50))
+		draw.RoundedBox(8, 15 + 2.5, surface.ScreenHeight() * 0.8565 + 6, surface.ScreenWidth() * 0.15 * (LocalPlayer():Health() / LocalPlayer():GetMaxHealth()) - 5, surface.ScreenHeight() * 0.03 - 5, Color(167, 15,75))
+		draw.SimpleText(LocalPlayer():Health(), "HUDFont", surface.ScreenWidth() * 0.085, surface.ScreenHeight() * 0.863, color_white, TEXT_ALIGN_CENTER )
 
-		draw.RoundedBox(8, 15, surface.ScreenHeight() * 0.89, surface.ScreenWidth() * 0.15, surface.ScreenHeight() * 0.03, Color(222,16,50))
-		draw.RoundedBox(8, 15 + 2.5, surface.ScreenHeight() * 0.886 + 6, surface.ScreenWidth() * 0.15 * (LocalPlayer():Armor() / 100) - 5, surface.ScreenHeight() * 0.03 - 5, Color(15, 75,167))
-		draw.SimpleText(LocalPlayer():Armor(), "HUDFont", surface.ScreenWidth() * 0.085, surface.ScreenHeight() * 0.894, color_white, TEXT_ALIGN_CENTER )
+		draw.RoundedBox(8, 15, surface.ScreenHeight() * 0.9, surface.ScreenWidth() * 0.15, surface.ScreenHeight() * 0.03, Color(222,16,50))
+		draw.RoundedBox(8, 15 + 2.5, surface.ScreenHeight() * 0.897 + 6, surface.ScreenWidth() * 0.15 * (LocalPlayer():Armor() / 100) - 5, surface.ScreenHeight() * 0.03 - 5, Color(15, 75,167))
+		draw.SimpleText(LocalPlayer():Armor(), "HUDFont", surface.ScreenWidth() * 0.085, surface.ScreenHeight() * 0.905, color_white, TEXT_ALIGN_CENTER )
 
 
 		-- Main Hud
@@ -61,11 +61,11 @@ hook.Add("HUDPaint", "HUDHOOK", function()
 		-- XP and Round
 
 		if (IsValid(LocalPlayer():GetActiveWeapon())) then
-			draw.RoundedBox(4, surface.ScreenWidth() * 0.815, surface.ScreenHeight() * 0.75, surface.ScreenWidth() * 0.175, surface.ScreenHeight() * 0.19, Color(222,16,50))
-			draw.RoundedBox(4 + 4, surface.ScreenWidth() * 0.815 + 2.5, surface.ScreenHeight() * 0.746 + 6, surface.ScreenWidth() * 0.175 - 5, surface.ScreenHeight() * 0.19 - 5, Color(45,45,45))
+			draw.RoundedBox(4, surface.ScreenWidth() * 0.815, surface.ScreenHeight() * 0.77, surface.ScreenWidth() * 0.175, surface.ScreenHeight() * 0.19, Color(222,16,50))
+			draw.RoundedBox(4 + 4, surface.ScreenWidth() * 0.815 + 2.5, surface.ScreenHeight() * 0.767 + 6, surface.ScreenWidth() * 0.175 - 5, surface.ScreenHeight() * 0.19 - 5, Color(45,45,45))
 
-			draw.SimpleText(LocalPlayer():GetActiveWeapon():GetPrintName(), "HUDFontWeapon", surface.ScreenWidth() * 0.9, surface.ScreenHeight() * 0.8, color_white, TEXT_ALIGN_CENTER )
-			draw.DrawText( LocalPlayer():GetActiveWeapon():Clip1() .. " / " .. LocalPlayer():GetAmmoCount(LocalPlayer():GetActiveWeapon():GetPrimaryAmmoType()), "HUDFontWeapon", surface.ScreenWidth() * 0.9, surface.ScreenHeight() * 0.84, Color(255,255,255,255), TEXT_ALIGN_CENTER)
+			draw.SimpleText(LocalPlayer():GetActiveWeapon():GetPrintName(), "HUDFontWeapon", surface.ScreenWidth() * 0.9, surface.ScreenHeight() * 0.83, color_white, TEXT_ALIGN_CENTER )
+			draw.DrawText( LocalPlayer():GetActiveWeapon():Clip1() .. " / " .. LocalPlayer():GetAmmoCount(LocalPlayer():GetActiveWeapon():GetPrimaryAmmoType()), "HUDFontWeapon", surface.ScreenWidth() * 0.9, surface.ScreenHeight() * 0.86, Color(255,255,255,255), TEXT_ALIGN_CENTER)
 		end
 
 		-- Weapon
