c9df27542a36200d11da20cc466d8a41c4b83a8d
Gwavity <72534647+Gwavity@users.noreply.github.com>
2022-12-30 22:38:43 -0500

    Update sv_money.lua

--- gamemode/sv_money.lua
+++ gamemode/sv_money.lua
@@ -1,9 +1,23 @@
 hook.Add("OnNPCKilled", "OnKill", function(npc, attacker, inflictor)
-
-    if(attacker:GetNWInt("PlayerPrestige") >= 10) then
-        attacker:SetNWInt("MoneyAmount",attacker:GetNWInt("MoneyAmount") + 200 * attacker:GetNWInt("RoundNumberVar"))
-    else 
-        attacker:SetNWInt("MoneyAmount",attacker:GetNWInt("MoneyAmount") + 100 * attacker:GetNWInt("RoundNumberVar"))
+    if npc:GetClass() == "npc_vj_zs_zombie" then
+        attacker:SetNWInt("CurrentMoney",100)
     end
 
+    if npc:GetClass() == "npc_vj_zs_fastzombie" then
+        attacker:SetNWInt("CurrentMoney",200)
+    end
+
+    if npc:GetClass() == "npc_vj_zs_poisonzombie" then
+        attacker:SetNWInt("CurrentMoney",300)
+    end
+
+    if npc:GetClass() == "npc_vj_zs_zombine" then
+        attacker:SetNWInt("CurrentMoney",400)
+    end
+
+    if(attacker:GetNWInt("PlayerPrestige") >= 10) then
+        attacker:SetNWInt("MoneyAmount",attacker:GetNWInt("MoneyAmount") + ((attacker:GetNWInt("CurrentMoney") * 2) * attacker:GetNWInt("RoundNumberVar")))
+    else 
+        attacker:SetNWInt("MoneyAmount",attacker:GetNWInt("MoneyAmount") + (attacker:GetNWInt("CurrentMoney") * attacker:GetNWInt("RoundNumberVar")))
+    end
 end)
