0139a5b6ed6498a005ee01eda12c48cff1c81521
Gwavity <72534647+Gwavity@users.noreply.github.com>
2023-01-01 17:43:50 -0500

    Update sv_hit.lua

--- gamemode/sv_hit.lua
+++ gamemode/sv_hit.lua
@@ -6,6 +6,6 @@ hook.Add("EntityTakeDamage", "SendHit", function(target,dmginfo)
     if !target:IsNPC() then return end
     if dmginfo:GetAttacker():IsPlayer() then
         net.Start("SendAttacker")
-        net.Broadcast()
+        net.Send(dmginfo:GetAttacker())
     end
 end)
