ca46068bf2e765a06eef24df80c695b8a05a4caf
Gwavity <72534647+Gwavity@users.noreply.github.com>
2023-01-01 17:34:37 -0500

    Update cl_scoreboard.lua

--- gamemode/cl_scoreboard.lua
+++ gamemode/cl_scoreboard.lua
@@ -26,11 +26,12 @@ local function CreateScoreboard(ToggleScoreboard)
             surface.DrawRect(0, 0, w, h)
             surface.SetDrawColor(222,16,50)
             surface.DrawOutlinedRect(0, 0, w, h)
-            draw.SimpleText("Name", "HUDFont", w * 0.153 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.SimpleText("Money", "HUDFont", w * 0.384 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.SimpleText("Prestige", "HUDFont", w * 0.587 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.SimpleText("Rank", "HUDFont", w * 0.735 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.SimpleText("Ping", "HUDFont", w * 0.868 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            draw.SimpleText("Name", "HUDFont", w * 0.12, h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            draw.SimpleText("Money", "HUDFont", w * 0.3 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            draw.SimpleText("Prestige", "HUDFont", w * 0.45 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            draw.SimpleText("Rank", "HUDFont", w * 0.6 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            draw.SimpleText("Wins", "HUDFont", w * 0.75 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            draw.SimpleText("Ping", "HUDFont", w * 0.9 , h * 0.03 , Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         end
 
         local ypos = 0
@@ -46,19 +47,20 @@ local function CreateScoreboard(ToggleScoreboard)
             local PlayerMoney = v:GetNWInt("MoneyAmount")
             local PlayerPrestige = v:GetNWInt("PlayerPrestige")
             local PlayerRank = v:GetNWInt("PlayerRank")
-            local PlayerPing = v:Ping()
+	    local PlayerWins = 0--v:GetNWInt("PlayerWins") later most likely and have it save on each win
+            local PlayerPing = v:Ping()'
             
             if v:GetNWString("BackgroundColor") == "" then v:SetNWString("BackgroundColor", tostring(ColorRand())) end
             PlayerPanel.Paint = function(self,w,h)
                 if IsValid(v) then
                     surface.SetDrawColor(222,16,50)
                     surface.DrawRect(0, 0, w, h)
-                    draw.SimpleText(PlayerName, "HUDFont", w * 0.153 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-                    draw.SimpleText("$"..tostring(PlayerMoney), "HUDFont", w * 0.384 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-                    draw.SimpleText(PlayerPrestige, "HUDFont", w * 0.587 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-                    draw.SimpleText(PlayerRank, "HUDFont", w * 0.735 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-                    draw.SimpleText(PlayerPing, "HUDFont", w * 0.868 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-
+                    draw.SimpleText(PlayerName, "HUDFont", w * 0.12 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                    draw.SimpleText("$"..tostring(PlayerMoney), "HUDFont", w * 0.3 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                    draw.SimpleText(PlayerPrestige, "HUDFont", w * 0.45 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                    draw.SimpleText(PlayerRank, "HUDFont", w * 0.6 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                    draw.SimpleText(PlayerWins, "HUDFont", w * 0.75 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                    draw.SimpleText(PlayerPing, "HUDFont", w * 0.9 , h /2 , Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
                 end
             end
             ypos = ypos + PlayerPanel:GetTall() * 1.1
