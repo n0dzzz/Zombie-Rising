79fb02337d3218c465388a271521a877ad221cf1
Gwavity <72534647+Gwavity@users.noreply.github.com>
2023-01-08 00:59:03 -0500

    Update cl_hud.lua

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,6 +1,6 @@
-AddCSLuaFile"cl_init.lua"
-AddCSLuaFile"shared.lua"
-include"shared.lua"
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
 
 surface.CreateFont("HUDFont", {
 	font = "Segoe UI Bold",
