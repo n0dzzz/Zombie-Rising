d232210727ea935179feff72baef4261087f5138
Gwavity <72534647+Gwavity@users.noreply.github.com>
2023-01-08 02:01:06 -0500

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,6 +4,7 @@ AddCSLuaFile( "cl_weaponbox.lua" )
 AddCSLuaFile( "cl_hud.lua" )
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "cl_spectatehud.lua" )
+AddCSLuaFile( "cl_scoreboard.lua" )
 AddCSLuaFile( "cl_hit.lua" )
 
 GM.Name = "Zombie Surival"
